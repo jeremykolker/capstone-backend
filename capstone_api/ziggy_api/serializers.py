82d0c51d4186c4c5882116108e25cc2d49cce2c7
“Jeremy <jeremy.kolker@gmail.com>
2023-04-30 15:05:14 -0400

    updated models/database

--- capstone_api/ziggy_api/serializers.py
+++ capstone_api/ziggy_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Item
 class ItemSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Item # tell django which model to use
-        fields = ('id', 'photo', 'name', 'color', 'price', 'size', 'description')
+        fields = ('id', 'photo_url', 'name', 'color', 'price', 'size', 'description')
